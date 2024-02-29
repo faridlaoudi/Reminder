5fe538ede32bcc6bd28bd6548dfdfcb60f253007
Farid LAOUDI <117023870+faridlaoudi@users.noreply.github.com>
2024-02-29 09:11:38 +0100

    notification setting

---
+++ Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Timer1 = New Timer(components)
+        Timer2 = New Timer(components)
+        Timer3 = New Timer(components)
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        MaskedTextBox1 = New MaskedTextBox()
+        ComboBox1 = New ComboBox()
+        NotifyIcon1 = New NotifyIcon(components)
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(25, 32)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 20)
+        Label1.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(25, 89)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(42, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Time"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(25, 152)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(70, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Message:"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        ' 
+        ' Timer3
+        ' 
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(25, 175)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(280, 78)
+        TextBox1.TabIndex = 4
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(110, 296)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 5
+        Button1.Text = "Start"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Enabled = False
+        Button2.Location = New Point(110, 331)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 6
+        Button2.Text = "Stop"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(211, 355)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 7
+        Button3.Text = "notify"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' MaskedTextBox1
+        ' 
+        MaskedTextBox1.Location = New Point(25, 112)
+        MaskedTextBox1.Mask = "00:00:00"
+        MaskedTextBox1.Name = "MaskedTextBox1"
+        MaskedTextBox1.Size = New Size(179, 27)
+        MaskedTextBox1.TabIndex = 8
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"AM", "PM"})
+        ComboBox1.Location = New Point(211, 112)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(94, 28)
+        ComboBox1.TabIndex = 9
+        ' 
+        ' NotifyIcon1
+        ' 
+        NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+        NotifyIcon1.BalloonTipText = "reminder"
+        NotifyIcon1.Text = "NotifyIcon1"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(25, 256)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(0, 20)
+        Label4.TabIndex = 10
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(322, 400)
+        Controls.Add(Label4)
+        Controls.Add(ComboBox1)
+        Controls.Add(MaskedTextBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MinimizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Label4 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+    ' Using DateTime for accurate time comparison.
+    Dim currentTime As DateTime
+    Dim messageTime As DateTime
+    Dim reminderShown As Boolean = False
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' Updating the current time display.
+        UpdateCurrentTime()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize anything if needed, for example, setting up initial states of controls.
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        ' Checking if it's time to show the reminder.
+        CheckReminder()
+    End Sub
+
+    Private Sub UpdateCurrentTime()
+        ' Gets the current time and updates the label.
+        currentTime = DateTime.Now
+        Label1.Text = currentTime.ToString("hh:mm:ss tt")
+    End Sub
+
+    Private Sub CheckReminder()
+        ' Checks if the current time is equal or has passed the message time and if the reminder hasn't been shown yet.
+        If Not reminderShown AndAlso currentTime >= messageTime Then
+            ShowReminder()
+        End If
+    End Sub
+
+    Private Sub ShowReminder()
+        ' Stops the timer, shows the message, and updates the UI accordingly.
+        Timer3.Stop()
+        MsgBox(TextBox1.Text)
+        Button1.Enabled = True
+        Button2.Enabled = False
+        Label4.Text = ""
+        reminderShown = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Parses user input to set the reminder time and starts the timer.
+        SetReminder()
+        Timer3.Start()
+        Button1.Enabled = False
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Stops the reminder.
+        Timer3.Stop()
+        Button1.Enabled = True
+        Button2.Enabled = False
+        Label4.Text = ""
+        reminderShown = False ' Allows the reminder to be set again.
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Minimizes the application to the system tray and shows a balloon tip.
+        MinimizeToTray()
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        ' Restores the application from the system tray.
+        RestoreFromTray()
+    End Sub
+
+    Private Sub SetReminder()
+        ' Assuming MaskedTextBox1 contains the time in HH:MM format and ComboBox1 contains AM/PM.
+        Dim parsedTime As DateTime
+        If DateTime.TryParseExact(MaskedTextBox1.Text & " " & ComboBox1.Text, "hh:mm:ss tt", Nothing, Globalization.DateTimeStyles.None, parsedTime) Then
+            ' Set the message time for today with the specified time.
+            messageTime = DateTime.Today.Add(parsedTime.TimeOfDay)
+            Label4.Text = "Reminder set for: " & messageTime.ToString("hh:mm:ss tt")
+            reminderShown = False ' Ensures the reminder can trigger.
+        Else
+            MessageBox.Show("Invalid time format. Please enter the time as HH:MM.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub MinimizeToTray()
+        NotifyIcon1.Visible = True
+        Me.Hide()
+        NotifyIcon1.ShowBalloonTip(3000)
+    End Sub
+
+    Private Sub RestoreFromTray()
+        Me.Show()
+        NotifyIcon1.Visible = False
+    End Sub
+
+End Class
